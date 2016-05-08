bc39d10e5bc4d769cc48ea5d6093ad32462ddeb1
jiimms <jiimms@yahoo.com>
2016-05-08 10:33:51 +0530

    log-in log-out and remember me done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user =User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  		redirect_to user
+  	else
+      flash[:danger] ="Invalid email or password"
+      redirect_to login_path
+      #flash disappears after one request but render is not considered 
+      #a request so flash persists. solution is flash.now
+      #flash.now[:danger] = 'Invalid email/password combination'
+  		# render 'new'
+  	end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
   get "page" => 'static_pages#page'
 
   get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
